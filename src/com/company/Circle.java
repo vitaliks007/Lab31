01337b8e12256e2699470fbb611ab2645b13c8f1
VPlay <svitalik2002@gmail.com>
2021-09-05 17:53:31 +0300

    Lab31

---
+++ src/com/company/Circle.java
@@ -0,0 +1,44 @@
+package com.company;
+
+public class Circle {
+    private int radius, x, y;
+
+    public Circle(int radius, int x, int y) {
+        this.radius = radius;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
---
+++ src/com/company/CircleTest.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class CircleTest {
+    public static void main(String[] args) {
+        // write your code here
+        Circle circle = new Circle(1, 0, 0);
+        System.out.println(circle);
+    }
+}
